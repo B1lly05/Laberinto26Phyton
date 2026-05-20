3832e8cab424bd704c1aa4ab54592eb5b1a7a408
B1lly05 <billalchal446@gmail.com>
2026-05-20 19:25:44 +0200

    Delete 1ºEntrega directory

--- 1ºEntrega/Python/Laberinto26/Ente.py
+++
@@ -1,9 +0,0 @@
-class Ente_Clase():
-    def __init__(self):
-        self.poder = 10
-        self.posicion = None
-        self.vidas = 50
-
-    def estaVivo(self):
-        return self.vidas > 0
-     
